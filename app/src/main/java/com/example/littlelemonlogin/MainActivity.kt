b5e5d5a792cb0955bfb5472a3e8c5e8fe1e0bf6b
Mari <marygt182@gmail.com>
2024-01-03 16:01:41 +0000

    Make text fields accept and show the input

--- app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
+++ app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
@@ -16,11 +18,20 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.littlelemonlogin.ui.theme.LittleLemonLoginTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,26 +56,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen() {
+
+    var username by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.littlelemonlogo),
-            contentDescription = "Logo Image"
+            contentDescription = "Logo Image",
+            modifier = Modifier.padding(10.dp)
         )
 
-        TextField(value = "", onValueChange = {}, label = {
-            Text(text = "Username")
-        })
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            modifier = Modifier.padding(10.dp),
+            label = {
+                Text(text = "Username")
+            })
 
-        TextField(value = "", onValueChange = {}, label = {
-            Text(text = "Password")
-        })
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier.padding(10.dp),
+            visualTransformation = PasswordVisualTransformation(),
+            label = {
+                Text(text = "Password")
+            })
 
 
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { /*TODO*/ }, modifier = Modifier.padding(10.dp),
             colors = ButtonDefaults.buttonColors(Color(0xFF495E57))
         ) {
             Text(text = "Login", color = Color(0xFFEDEFEE))
@@ -73,7 +98,6 @@ fun LoginScreen() {
     }
 
 }
-
 
 @Preview(showBackground = true)
 @Composable
